f5aab7ac7fc438d125e1ce7508a0a69f0fdb05c7
Mehni <Mehni@users.noreply.github.com>
2018-08-06 12:58:52 +0200

    documentation and code clarity

--- AllYourBase/XMLSharedBaseDetection.cs
+++ AllYourBase/XMLSharedBaseDetection.cs
@@ -12,33 +12,29 @@ namespace AllYourBase
     internal static class XmlSharedBaseDetection
     {
         /// <summary>
-        /// INTENT: Decrease compatibility issues caused by modders overwriting abstract bases, by making it easy to detect the existence of these bad practices.
+        /// INTENT: Decrease compatibility issues caused by modders overwriting (abstract) bases, by making it easy to detect the existence of these bad practices.
         /// 
-        /// Run once on startup. Creates list of all abstract bases in vanilla, then creates list of all abstract bases in mods. Warns with mod and filename for any matches found. 
+        /// Run once on startup. Creates list of all (abstract) bases in vanilla, then compares with bases in mods. Warns with mod name, base and exact filename for any matches found. 
         /// </summary>
         static XmlSharedBaseDetection()
         {
-            //get all abstract bases in vanilla.
+            if (!Prefs.DevMode) return;
+            //get all bases in vanilla.
             List<string> vanillaXmlAttributes = new List<string>();
 
             List<LoadableXmlAsset> allVanillaLoadableXmlAssets = LoadedModManager.LoadModXML().Where(x => x.mod.IsCoreMod).ToList();
-           
 
             foreach (LoadableXmlAsset asset in allVanillaLoadableXmlAssets)
             {
-                if (asset.xmlDoc?.DocumentElement != null)
+                if (asset.xmlDoc?.DocumentElement == null) continue;
+                XmlNodeList childNodes = asset.xmlDoc.DocumentElement.ChildNodes;
                 {
-                    XmlNodeList childNodes = asset.xmlDoc.DocumentElement.ChildNodes;
+                    for (int i = 0; i < childNodes.Count; i++)
                     {
-                        for (int i = 0; i < childNodes.Count; i++)
+                        if (childNodes[i].NodeType != XmlNodeType.Element) continue;
+                        if (childNodes[i]?.Attributes?["Name"] != null)
                         {
-                            if (childNodes[i].NodeType == XmlNodeType.Element)
-                            {
-                                if (childNodes[i]?.Attributes?["Name"] != null)
-                                {
-                                    vanillaXmlAttributes.Add(childNodes[i].Attributes.GetNamedItem("Name").Value);
-                                }
-                            }
+                            vanillaXmlAttributes.Add(childNodes[i].Attributes.GetNamedItem("Name").Value);
                         }
                     }
                 }
@@ -50,19 +46,16 @@ namespace AllYourBase
 
             foreach (LoadableXmlAsset allModdedXmlAsset in allModdedXmlAssets)
             {
-                if (allModdedXmlAsset.xmlDoc?.DocumentElement != null)
+                if (allModdedXmlAsset.xmlDoc?.DocumentElement == null) continue;
+                XmlNodeList childNodes = allModdedXmlAsset.xmlDoc.DocumentElement.ChildNodes;
                 {
-                    XmlNodeList childNodes = allModdedXmlAsset.xmlDoc.DocumentElement.ChildNodes;
+                    for (int i = 0; i < childNodes.Count; i++)
                     {
-                        for (int i = 0; i < childNodes.Count; i++)
+                        if (childNodes[i].NodeType != XmlNodeType.Element) continue;
+                        if (childNodes[i]?.Attributes?["Name"] != null && vanillaXmlAttributes.Contains(childNodes[i].Attributes.GetNamedItem("Name").Value))
                         {
-                            if (childNodes[i].NodeType == XmlNodeType.Element)
-                            {
-                                if (childNodes[i]?.Attributes?["Name"] != null && vanillaXmlAttributes.Contains(childNodes[i].Attributes.GetNamedItem("Name").Value))
-                                {
-                                    Log.Warning(allModdedXmlAsset.mod.Name + " overwrites " + childNodes[i].Attributes.GetNamedItem("Name").Value + " in file "+ allModdedXmlAsset.FullFilePath + " causing compatibility errors.");
-                                }
-                            }
+                            Log.Warning("[" + allModdedXmlAsset.mod.Name + "]" + " causes compatibility errors by overwriting " + childNodes[i].Attributes.GetNamedItem("Name").Value + " in file "
+                                      + allModdedXmlAsset.FullFilePath);
                         }
                     }
                 }
