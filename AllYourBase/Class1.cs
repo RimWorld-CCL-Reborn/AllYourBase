82ad2d190aa3055ee71779a7d2621bc3acfeb68c
Mehni <Mehni@users.noreply.github.com>
2018-08-06 10:21:19 +0200

    Add project files.

---
+++ AllYourBase/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Harmony;
+
+namespace AllYourBase
+{
+    public class Class1
+    {
+    }
+}
