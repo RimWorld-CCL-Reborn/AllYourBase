b1a8fc9b1d995c81d1ccf88fe64345f506eddd2b
Mehni <Mehni@users.noreply.github.com>
2018-11-12 15:41:41 +0100

    Option to (attempt) cleanup of bad mods.
    If Options => Enable Verbose Logging is enabled, GO NUCLEAR!
    
    Use with caution. Ask a responsible adult for guidance.

--- AllYourBase/XMLSharedBaseDetection.cs
+++ AllYourBase/XMLSharedBaseDetection.cs
@@ -23,7 +23,7 @@ namespace AllYourBase
             Log.ResetMessageCount();
 
             //get all bases in vanilla.
-            List<string> vanillaXmlAttributes = new List<string>();
+            Dictionary<string, string> vanillaXmlAttributes = new Dictionary<string, string>();
 
             foreach (ModContentPack mod in LoadedModManager.RunningMods.Where(mod => mod.IsCoreMod))
             {
@@ -37,33 +37,47 @@ namespace AllYourBase
                             if (childNodes[i].NodeType != XmlNodeType.Element) continue;
                             if (childNodes[i]?.Attributes?["Name"] != null)
                             {
-                                vanillaXmlAttributes.Add(childNodes[i].Attributes.GetNamedItem("Name").Value);
+                                vanillaXmlAttributes.Add(childNodes[i].Attributes.GetNamedItem("Name").Value, childNodes[i].Name);
                             }
                         }
                     }
                 }
             }
 
-            //get all abstract bases in mods and compare them to the vanilla list.
+            if (Prefs.LogVerbose)
+                Log.Error("Verbose mode detected. AllYourBase will attempt to fix compatibility errors. If problem persists or gets worse, Verify File Integrity or redownload affected mods.", true);
+
+            //get all bases in mods and compare them to the vanilla list.
             foreach (ModContentPack mod in LoadedModManager.RunningMods.Where(mod => !mod.IsCoreMod))
             {
                 foreach (LoadableXmlAsset asset in DirectXmlLoader.XmlAssetsInModFolder(mod, "Defs/"))
                 {
+                    bool dirty = false;
                     if (asset.xmlDoc?.DocumentElement == null) continue;
                     XmlNodeList childNodes = asset.xmlDoc.DocumentElement.ChildNodes;
                     {
-                        for (int i = 0; i < childNodes.Count; i++)
+                        for (int i = childNodes.Count -1 ; i >= 0; i--)
                         {
                             if (childNodes[i].NodeType != XmlNodeType.Element) continue;
 
                             if (childNodes[i]?.Attributes?["Name"] != null &&
-                                vanillaXmlAttributes.Contains(childNodes[i].Attributes.GetNamedItem("Name").Value))
+                                vanillaXmlAttributes.ContainsKey(childNodes[i].Attributes.GetNamedItem("Name").Value)
+                                && vanillaXmlAttributes[childNodes[i].Attributes.GetNamedItem("Name").Value] == childNodes[i].Name)
                             {
                                 Log.Warning("[" + asset.mod.Name + "]" + " causes compatibility errors by overwriting " +
-                                            childNodes[i].Attributes.GetNamedItem("Name").Value + " in file " + asset.FullFilePath);
+                                            childNodes[i].Attributes.GetNamedItem("Name").Value + " in file " + asset.FullFilePath, true);
+
+                                if (Prefs.LogVerbose)
+                                {
+                                    Log.Message($"Attempting fix for {childNodes[i].Attributes.GetNamedItem("Name").Value}", true);
+                                    dirty = true;
+                                    asset.xmlDoc.DocumentElement.RemoveChild(childNodes[i]);
+                                }
                             }
                         }
                     }
+                    if (dirty)
+                        asset.xmlDoc.Save(asset.FullFilePath);
                 }
             }
 
